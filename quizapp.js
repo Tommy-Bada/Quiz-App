e835aeb4623d55c9aef26ebe4f04b67efbfc2f43
Bada tomiwa <tomiwabd@gmail.com>
2022-06-17 15:44:11 -0400

    Styled

---
+++ quizapp.js
@@ -0,0 +1,6 @@
+// for( let index = 0; index < 10; index++){
+//     let optionButton = document.getElementsByTagName("button");
+//     optionButton[index].addEventListener("click", function priceHover(){
+//         optionButton.style.backgroundcolor = "red";
+//     })
+// }
