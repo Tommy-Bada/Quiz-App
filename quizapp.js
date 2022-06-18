a27ae5b0b9d2f15a3674f79d5fe509c48fb829da
Bada tomiwa <tomiwabd@gmail.com>
2022-06-18 11:18:02 -0400

    Click and button

--- quizapp.js
+++ quizapp.js
@@ -1,8 +1,10 @@
-var nextButtons = document.getElementsByClassName('nxt');
-var questionBox = document.getElementsByClassName('question')
 
+var nextButtons = document.getElementsByClassName('nxt');
 for (let i = 0; i < nextButtons.length; i++) {
     nextButtons[i].addEventListener("click", function scrollRight(){  
+        let questionBox = document.getElementsByClassName('question')
             questionBox[i].style.marginLeft = "-98vw";
     })
 }
+
+var options = document.getElementByclass('option')
