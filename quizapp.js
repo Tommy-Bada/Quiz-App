5cd0c53ffc208e556093cddb4861f5f4ecaaaac9
Bada tomiwa <tomiwabd@gmail.com>
2022-06-19 11:54:06 -0400

    new styling

--- quizapp.js
+++ quizapp.js
@@ -1,10 +1,20 @@
 
-var nextButtons = document.getElementsByClassName('nxt');
-for (let i = 0; i < nextButtons.length; i++) {
-    nextButtons[i].addEventListener("click", function scrollRight(){  
-        let questionBox = document.getElementsByClassName('question')
-            questionBox[i].style.marginLeft = "-98vw";
-    })
-}
+// var nextButtons = document.getElementsByClassName('nxt');
+// for (let i = 0; i < nextButtons.length; i++) {
+//     nextButtons[i].addEventListener("click", function scrollRight(){  
+//         let questionBox = document.getElementsByClassName('question')
+//             questionBox[i].style.marginLeft = "-98vw";
+//     })
+// }
 
-var options = document.getElementByclass('option')
+// var options = document.getElementsByClassName('option');
+//     options[i].addEventListener("click", function scrollRight(i){  
+//         let questionBox = document.getElementsByClassName('question')
+//             questionBox[i].style.marginLeft = "-98vw";
+//     })
+
+    var startButton = document.getElementById("start-button")
+        startButton.addEventListener("click", function scrollRight(){  
+            let introPage = document.getElementById("intro")
+            introPage.style.marginLeft = "-100vw";
+        })
